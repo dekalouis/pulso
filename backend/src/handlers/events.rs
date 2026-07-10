1a0ef3bcaef4d2c2c238a5c1eae3ced899f14e3f
Deka Louis <dekarebaudo@gmail.com>
2026-07-10 22:26:17 +0700

    feat: postgres start for the backend

--- backend/src/handlers/events.rs
+++ backend/src/handlers/events.rs
@@ -1,11 +1,26 @@
 // the actual logic (like controllers)
 
-use axum::Json;
+use axum::{extract::State, Json};
+use sqlx::PgPool;
 use crate::models::event::Event;
 
-pub async fn create_event(Json(payload): Json<Event>) -> &'static str {
-    println!("Received event: {:?}", payload);
-    "Event received"
+// pub async fn create_event(Json(payload): Json<Event>) -> &'static str {
+//     println!("Received event: {:?}", payload);
+//     "Event received"
+// }
+
+pub async fn create_event(
+    State(pool): State<PgPool>,
+    Json(payload): Json<Event>,
+) -> &'static str {
+    sqlx::query("INSERT INTO events (tenant_id, event_type) VALUES ($1, $2)")
+        .bind(&payload.tenant_id)
+        .bind(&payload.event_type)
+        .execute(&pool)
+        .await
+        .expect("Failed to insert event");
+
+    "Event stored"
 }
 
 pub async fn health_check() -> &'static str {
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,12 +5,23 @@ mod handlers;
 
 use axum::{routing::{get, post}, Router};
 use handlers::events::{create_event, health_check};
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let app = Router::new()
         .route("/events", post(create_event))
-        .route("/health", get(health_check));
+        .route("/health", get(health_check))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on http://localhost:3000");
