7244a2312a25634d4a5ad198a8c8980bf33a042a
Deka Louis <dekarebaudo@gmail.com>
2026-07-10 23:22:32 +0700

    chore: stop tracking build artifacts

--- backend/target/debug/deps/backend-801381aa72e92cf4.d
+++
@@ -1,5 +0,0 @@
-/home/dekalouis/Developer/pulso/backend/target/debug/deps/backend-801381aa72e92cf4.d: src/main.rs
-
-/home/dekalouis/Developer/pulso/backend/target/debug/deps/backend-801381aa72e92cf4: src/main.rs
-
-src/main.rs:
